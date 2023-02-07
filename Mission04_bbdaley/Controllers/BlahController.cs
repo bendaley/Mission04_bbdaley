d4d6f746cb2e382391e7bee33f1e7c9620a57792
bendaley <bbdaley21@gmail.com>
2023-02-06 18:36:23 -0700

    Commit 2

---
+++ Mission04_bbdaley/Controllers/BlahController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_bbdaley.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
